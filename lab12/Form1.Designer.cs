c894e11104ae9b2e9d6abf57f2ec81095735221c
galina-zs <zab.galina.24@gmail.com>
2021-05-17 16:42:17 +0300

    Task is done 8 teams are participating. 7 rounds in the match, in each round all teams play 1 time

---
+++ lab12/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextTourButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cleanButton = new System.Windows.Forms.Button();
+            this.tourLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nextTourButton
+            // 
+            this.nextTourButton.Location = new System.Drawing.Point(12, 290);
+            this.nextTourButton.Name = "nextTourButton";
+            this.nextTourButton.Size = new System.Drawing.Size(105, 23);
+            this.nextTourButton.TabIndex = 1;
+            this.nextTourButton.Text = "Следующий тур";
+            this.nextTourButton.UseVisualStyleBackColor = true;
+            this.nextTourButton.Click += new System.EventHandler(this.nextTourButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(666, 223);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Команда";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 77;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Игры";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 59;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Выигрыши";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 86;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "Ничьи";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 63;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column5.HeaderText = "Проигрыши";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 91;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column6.HeaderText = "Забито";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 68;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column7.HeaderText = "Пропущено";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 90;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column8.HeaderText = "Очки";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 57;
+            // 
+            // cleanButton
+            // 
+            this.cleanButton.Location = new System.Drawing.Point(12, 331);
+            this.cleanButton.Name = "cleanButton";
+            this.cleanButton.Size = new System.Drawing.Size(105, 23);
+            this.cleanButton.TabIndex = 3;
+            this.cleanButton.Text = "Очистить";
+            this.cleanButton.UseVisualStyleBackColor = true;
+            this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+            // 
+            // tourLabel
+            // 
+            this.tourLabel.AutoSize = true;
+            this.tourLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tourLabel.Location = new System.Drawing.Point(146, 290);
+            this.tourLabel.Name = "tourLabel";
+            this.tourLabel.Size = new System.Drawing.Size(39, 20);
+            this.tourLabel.TabIndex = 4;
+            this.tourLabel.Text = "0 / 7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 368);
+            this.Controls.Add(this.tourLabel);
+            this.Controls.Add(this.cleanButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.nextTourButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button nextTourButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.Button cleanButton;
+        private System.Windows.Forms.Label tourLabel;
+    }
+}
+
---
+++ lab12/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab12
+{
+    public partial class Form1 : Form
+    {
+        private class TeamInfo
+        {
+            public TeamInfo(String name, double lambda)
+            {
+                this.name = name;
+                this.lambda = lambda;
+            }
+            public String name;
+            public int victories;
+            public int draws;
+            public int losses;
+            public int scored;
+            public int missed;
+            public int points;
+            public double lambda;
+        }
+        private TeamInfo[] teams = new TeamInfo[8];
+        private TeamInfo[] stat = new TeamInfo[8];
+
+        // Отображает кто с кем играл за все время
+        private bool[,] plays = new bool[8, 8];
+        private int tourNumber = 0;
+        public Form1()
+        {
+            InitializeComponent();            
+            SetDefaultState();          
+        }    
+        
+        public void SetDefaultState()
+        {
+            cleanButton.Enabled = false;
+            nextTourButton.Enabled = true;
+            teams[0] = new TeamInfo("ЗЕНИТ", 2);
+            teams[1] = new TeamInfo("КРАСНОДАР", 2);
+            teams[2] = new TeamInfo("ПФК ЦСКА", 1.5);
+            teams[3] = new TeamInfo("ДИНАМО-МОСКВА", 1.7);
+            teams[4] = new TeamInfo("РУБИН", 1.8);
+            teams[5] = new TeamInfo("СПАРТАК-МОСКВА", 1.9);
+            teams[6] = new TeamInfo("ЛОКОМОТИВ", 1.6);
+            teams[7] = new TeamInfo("ТЕРЕК", 1.7);            
+            tourNumber = 0;
+            tourLabel.Text = tourNumber.ToString() + " / 7";
+            FillDataGridView(teams);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i <= j)
+                    {
+                        plays[i, j] = true;
+                    }
+                    else
+                    {
+                        plays[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        private void FillDataGridView(TeamInfo[] mas)
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < 8; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1[0, i].Value = mas[i].name;
+                dataGridView1[1, i].Value = tourNumber;                
+                dataGridView1[2, i].Value = mas[i].victories;
+                dataGridView1[3, i].Value = mas[i].draws;
+                dataGridView1[4, i].Value = mas[i].losses;
+                dataGridView1[5, i].Value = mas[i].scored;
+                dataGridView1[6, i].Value = mas[i].missed;
+                dataGridView1[7, i].Value = mas[i].points;
+            }
+        }
+
+        private void nextTourButton_Click(object sender, EventArgs e)
+        {
+            tourNumber++;
+            PlayTour();
+            teams.CopyTo(stat, 0);
+            QuickSortingTeams(0, stat.Length - 1);
+            FillDataGridView(stat);
+            tourLabel.Text = tourNumber.ToString() + " / 7";
+            if (tourNumber == 7)
+            {
+                cleanButton.Enabled = true;
+                nextTourButton.Enabled = false;
+            }
+        }
+
+        private void PlayTour()
+        {
+            int playsNumber = 0;
+            // Отображает какая команда уже играла в текущем туре
+            bool[] currentPlays = new bool[8];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (!plays[i, j] && !currentPlays[i] && !currentPlays[j])
+                    {
+                        plays[i, j] = true;
+                        currentPlays[i] = true;
+                        currentPlays[j] = true;
+                        // Забитые i-ой командой мячи 
+                        int firstScored = PoissonDistribution(teams[i]);
+                        teams[i].scored += firstScored;
+                        teams[j].missed += firstScored;
+                        // Забитые j-ой командой мячи
+                        int secondScored = PoissonDistribution(teams[j]);
+                        teams[j].scored += secondScored;
+                        teams[i].missed += secondScored;
+
+                        if (firstScored > secondScored)
+                        {
+                            teams[i].victories++;
+                            teams[j].losses++;
+                        }
+                        else if (firstScored < secondScored)
+                        {
+                            teams[j].victories++;
+                            teams[i].losses++;
+                        }
+                        else
+                        {
+                            teams[j].draws++;
+                            teams[i].draws++;
+                        }
+                        // Подсчет итоговых очков
+                        teams[i].points = teams[i].victories * 3 + teams[i].draws * 1;
+                        teams[j].points = teams[j].victories * 3 + teams[j].draws * 1;
+                        playsNumber++;
+                        if (playsNumber == 4)
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int PoissonDistribution(TeamInfo firstTeam)
+        {
+            int m = 0;
+            double S = 0;
+            
+            while (S >= (-firstTeam.lambda))
+            {
+                m++;
+                Generator();
+                double alpha = R;
+                S += Math.Log(alpha);
+            }
+
+            return m;
+        }
+
+        // Быстрая сортировка по итоговым очкам
+        private void QuickSortingTeams(int first, int last)
+        {
+            TeamInfo pivot = stat[(last - first) / 2 + first];
+            TeamInfo temp;
+            int i = first, j = last;
+            while (i <= j)
+            {
+                while (stat[i].points > pivot.points && i <= last)
+                {
+                    i++;
+                }
+                while (stat[j].points < pivot.points && j >= first)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    temp = stat[i];
+                    stat[i] = stat[j];
+                    stat[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (j > first)
+            {
+                QuickSortingTeams(first, j);
+            }
+            if (i < last)
+            {
+                QuickSortingTeams(i, last);
+            }
+        }
+
+        double U = 1;
+        const int p = 5087; // большое простое число
+        int M = 2900;       // M = p - 3^n. Берем n = 7, потому что 3^8 > p
+        double R;
+        private void Generator() // метод вычетов. Модификация Коробова
+        {
+            R = U / p;
+            U = (U * M) % p;
+        }
+
+        private void cleanButton_Click(object sender, EventArgs e)
+        {
+            SetDefaultState();
+        }
+    }
+}
